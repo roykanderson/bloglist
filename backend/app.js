aa7a780d5a544fd03195d230dbc6a710fad7a380
Roy Anderson <royanderson.k@gmail.com>
2022-09-29 17:55:57 -0700

    add testing route

--- backend/app.js
+++ backend/app.js
@@ -30,9 +30,16 @@ app.use(cors())
 app.use(express.json())
 app.use(middleware.reqLogger)
 app.use(middleware.tokenExtractor)
+
 app.use('/api/login', loginRouter)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
+
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
